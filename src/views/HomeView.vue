bac17a6d20e155d181642aa4472e7055dea240b6
Haruki-0322 <libertyfork@gmail.com>
2022-05-20 13:52:05 +0900

    リンクを作成しました。

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,30 @@
 <template>
-  <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <h1 class="home">Home</h1>
+  <div class="link">
+    <a href="./quiz">Vueクイズへ</a>
+    <a href="./palette">Vueパレットへ</a>
+    <a href="./memo">Vueメモへ</a>
+  </div>
 </template>
+
+<style>
+.home {
+  width: 95%;
+  text-align: center;
+  border-bottom: solid 5px #366;
+}
+.link {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+a {
+  text-decoration: none;
+  color: rgb(74, 178, 143);
+  font-size: xx-large;
+  font-weight: bolder;
+}
+a:hover {
+  text-decoration: underline;
+}
+</style>
